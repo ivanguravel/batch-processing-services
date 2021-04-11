922aa6f3b085a1d46bfd4ce01e15a37e09706211
Ivan Zhuravel <ivanguravel@gmail.com>
2021-04-11 20:13:16 +0300

    small fix

--- batch-processing/src/main/java/com/ivzh/batchprocessing/JobScheduler.java
+++ batch-processing/src/main/java/com/ivzh/batchprocessing/JobScheduler.java
@@ -7,13 +7,14 @@ import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.Date;
 
 @Component
 public class JobScheduler {
+
+    private boolean enableRun = false;
 
     @Autowired
     private Job job;
@@ -22,8 +23,11 @@ public class JobScheduler {
 
     @Scheduled(fixedRate = 5_000)
     public void launchJob() throws Exception {
-        JobExecution jobExecution = jobLauncher
-                .run(job, new JobParametersBuilder().addDate("runDate", new Date()).toJobParameters());
-
+        if (enableRun) {
+            JobExecution jobExecution = jobLauncher
+                    .run(job, new JobParametersBuilder().addDate("runDate", new Date()).toJobParameters());
+        } else {
+            this.enableRun = true;
+        }
     }
 }
